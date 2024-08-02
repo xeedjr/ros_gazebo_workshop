9437c0818db44cf55ada4c1b089cd959d2df2f56
Bogdan Yarushevskyi <xeedjr@gmail.com>
2024-08-02 16:04:14 +0000

    new

--- data/project_ws/build.sh
+++
@@ -1,7 +0,0 @@
-source /opt/ros/humble/setup.bash
-
-colcon build --cmake-args -DBUILD_TESTING=ON
-
-. ./install/setup.sh
-
-ros2 launch ros_gz_example_bringup diff_drive.launch.py
--- data/project_ws/install_dep.sh
+++
@@ -1,5 +0,0 @@
-apt update
-source /opt/ros/humble/setup.bash
-rosdep init
-rosdep update
-rosdep install --from-paths src --ignore-src -r -i -y --rosdistro humble
