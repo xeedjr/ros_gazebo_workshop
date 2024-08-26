6f4f92090444e826660bdbb870473419019e1bc6
Bogdan Yarushevskyi <xeedjr@gmail.com>
2024-08-26 16:08:05 +0300

    add container entry point script

--- data/project_ws/build2.sh
+++ data/project_ws/build2.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+cd /root/data/project_ws
+
+apt update
+source /opt/ros/humble/setup.bash
+rosdep init
+rosdep update
+rosdep install --from-paths src --ignore-src -r -i -y --rosdistro humble
+
 source /opt/ros/humble/setup.bash
 
 colcon build --cmake-args -DBUILD_TESTING=ON
